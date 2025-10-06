f5bbb1b46fcdad2568c6b89525cb1de8f386fd07
Hayoru <em.chechetkin@yandex.ru>
2025-10-06 14:22:00 +0300

    PLLConfig

--- ClockIni.c
+++ ClockIni.c
@@ -5,18 +5,26 @@
 	
 	void ClockIni ()
 	{
-		
+		RCC->CR = 0;
 		RCC->CR |= RCC_CR_HSEON + RCC_CR_PLLON;
 		HSE_CheckSave();
 		
 	
+		RCC->CFGR |= RCC_CFGR_SWS_PLL;
+		RCC->CFGR |= RCC_CFGR_HPRE_0;
+		
+		
+		
 		return;
 	}
 	
 	void PLLIni ()
 	{
 	 
-	 RCC->PLLCFGR |= RCC_PLL
-	 
+	 RCC->PLLCFGR = 0;
+	 RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
+	 RCC->PLLCFGR |= 0x8 << RCC_PLLCFGR_PLLM_Pos;
+	 RCC->PLLCFGR |= 0x160 << RCC_PLLCFGR_PLLN_Pos;
+	 RCC->PLLCFGR |= 0x0 << RCC_PLLCFGR_PLLP_Pos;
 	 return;
 	}
